000b62c8076d7ef8e07de319ce98378844b49f52
PringleCPP <76829210+PringleCPP@users.noreply.github.com>
2021-06-05 09:56:36 -0400

    Added IP address to console commands.

--- start-server.bat
+++ start-server.bat
@@ -1,5 +1,11 @@
 @echo off
 cls
+
+set ip_address_string="IPv4 Address"
+rem Uncomment the following line when using older versions of Windows without IPv6 support (by removing "rem")
+rem set ip_address_string="IP Address"
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Server IP: %%f
+
 :start
 TerrariaServer.exe -config serverconfig.txt
 @echo.

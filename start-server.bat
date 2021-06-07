5aca3a600004818841912f68f488f6a427fb54fe
PringleCPP <76829210+PringleCPP@users.noreply.github.com>
2021-06-06 20:16:23 -0400

    New start server method.

--- start-server.bat
+++ start-server.bat
@@ -1,14 +1,61 @@
 @echo off
+title Wisp Terraria Server
+echo Terraria V1.4.2.3
+
+echo.
+
+echo 1              start-server
+echo 2              start-steam-private-server
+echo 3              start-steam-public-server
+
+echo.
+
+:choice
+set /P q=Choose startup method: 
+if /I "%q%" EQU "1" goto :startserv
+if /I "%q%" EQU "2" goto :steampriv
+if /I "%q%" EQU "3" goto :steampub
+goto choice
+
+:startserv
 cls
 
 set ip_address_string="IPv4 Address"
-rem Uncomment the following line when using older versions of Windows without IPv6 support (by removing "rem")
-rem set ip_address_string="IP Address"
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Server IP: %%f
 
-:start
+:start1
 TerrariaServer.exe -config serverconfig.txt
 @echo.
 @echo Restarting server...
 @echo.
-goto start
+goto start1
+
+:steampriv
+cls
+
+set ip_address_string="IPv4 Address"
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Server IP: %%f
+
+:start2
+TerrariaServer.exe -steam -lobby private -config serverconfig.txt -experimental
+@echo.
+@echo Restarting server...
+@echo.
+goto start2
+
+:steampub
+cls
+
+set ip_address_string="IPv4 Address"
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Server IP: %%f
+
+:start3
+TerrariaServer.exe -steam -lobby friends -config serverconfig.txt
+@echo.
+@echo Restarting server...
+@echo.
+goto start3
+
+
+pause
+exit
